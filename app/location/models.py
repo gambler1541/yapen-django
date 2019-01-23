c9e3542382818cfa87c73865c8fe831ab5362475
pmb <gambler1541@gmail.com>
2019-01-23 20:12:40 +0900

    location, sub_location, pension, room model 작성

--- app/location/models.py
+++ app/location/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=10)
-    location_number = models.IntegerField()
+    location_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 
 class Sub_location(models.Model):
@@ -11,7 +14,7 @@ class Sub_location(models.Model):
     location = models.ForeignKey('Location',
                                  on_delete=models.CASCADE,
                                  related_name='sub_location',)
-    sub_location_number = models.IntegerField()
+    sub_location_number = models.CharField(max_length=10)
 
 
 class Pensions(models.Model):
@@ -19,8 +22,35 @@ class Pensions(models.Model):
     sub_location = models.ForeignKey('Sub_location',
                                      on_delete=models.CASCADE,
                                      related_name='pension',)
-    address = models.TextField(max_length=100),
-    check_in = models.CharField(max_length=10),
-    check_out = models.CharField(max_length=10),
+    pension_img = models.ImageField(blank=True,)
+    address = models.TextField(max_length=100, blank=True,)
+    check_in = models.CharField(max_length=10, blank=True,)
+    check_out = models.CharField(max_length=10, blank=True,)
+    pick_up = models.TextField(max_length=100, blank=True,)
+    number_of_room = models.TextField(max_length=10, blank=True,)
+    notice = models.TextField(max_length=100, blank=True,)
+
+
+class Room(models.Model):
+    pension = models.ForeignKey('Pensions',
+                                on_delete=models.CASCADE,
+                                related_name='room',)
+    # 방 이미지
+    room_img = models.ImageField(blank=True,)
+    # 방 구조
+    room_structure = models.TextField(max_length=100, blank=True,)
+    # 방 크기
+    size = models.TextField(max_length=100, blank=True,)
+    # 기준/최대 인원
+    max_people = models.TextField(max_length=100, blank=True,)
+    # 구비시설
+    facilities = models.TextField(max_length=100, blank=True,)
+    # 객실 설명
+    room_info = models.TextField(max_length=300, blank=True,)
+
+
+
+
+
 
 
